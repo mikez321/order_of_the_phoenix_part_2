5855e434612483db94ce261d910524d7cc262617
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-24 15:21:25 -0600

    more refactor to move functions out of controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,5 @@
 class SearchController < ApplicationController
   def index
-    member_info = PotterService.new(params['house'])
-    @house = House.new(params['house'], member_info.parsed)
+    @house = House.new(params['house'])
   end
 end
--- app/poros/house.rb
+++ app/poros/house.rb
@@ -1,9 +1,9 @@
 class House
   attr_reader :name, :members
-  def initialize(house_name, member_info)
+  def initialize(house_name)
     @name = house_name
-    @member_info = member_info
     @members = []
+    @member_info = retrieve_member_info
     create_members
   end
 
@@ -11,6 +11,12 @@ class House
     @member_info.each do |individual_info|
       @members << Member.new(individual_info)
     end
+  end
+
+  private
+
+  def retrieve_member_info
+    member_info = PotterService.new(@name).parsed
   end
 
 end
